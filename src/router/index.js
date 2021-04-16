92c674198b84427615755581167cbb8712c0b940
langlang <997041674@qq.com>
2021-04-16 16:14:03 +0800

    登录修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
-})
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes: [
+    /* { path: "/", name: "Home", component: Home },
+    {
+      path: "/about",
+      name: "About",
+      component: () => import("../views/About.vue"),
+    }, */
+    {
+      path: "/",
+      name: "Login",
+      component: () => import("../views/Login.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="login-title">
+      <h1>
+        后台管理系统
+      </h1>
+    </div>
+    <div class="login-middle">
+      <div class="login-loge">
+        <img src="@/assets/img/loge.png" alt="" />
+      </div>
+      <div class="midcourt">
+
+      </div>
+      <div class="Login-frame">
+        <el-form
+          label-position="right"
+          label-width="80px"
+          :model="formLabelAlign"
+        >
+          <el-form-item label="账号">
+            <el-input v-model="formLabelAlign.name" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input
+              type="password"
+              v-model="formLabelAlign.region"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      formLabelAlign: {
+        name: "",
+        region: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
+};
+</script>
+<style lang="less" scope>
+.login-title {
+  margin-top: 100px;
+  h1 {
+    text-align: center;
+    color: red;
+  }
+}
+.login-middle {
+  width: 100%;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  .login-loge {
+    width: 100px;
+    img {
+      width: 100%;
+    }
+  }
+  .midcourt{
+    width: 160px;
+  }
+  .Login-frame{
+    width: 400px;
+  }
+}
+.el-button {
+  width: 100%;
+}
+</style>
